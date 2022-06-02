776e787ba3c90fe28f78467222145d4e93c8c953
mapo june <danpa725@cau.ac.kr>
2022-06-02 09:11:04 +0900

    Update: main app core logic and UI .

--- src/app.py
+++ src/app.py
@@ -1,9 +1,8 @@
 from tkinter import *
-from calendar import Calendar
+from calendar_ui import CalenderUI
 from automation import automation
 from load import load_dir
-from excel import get_column
-from system import _win_set_time
+from system import win_set_time
 from time import sleep
 
 AUTO_CONFIG = {
@@ -11,29 +10,40 @@ AUTO_CONFIG = {
     "exceldir": "NOT",
 }
 
-is_config_setup_finishied = (
-    AUTO_CONFIG["slected_date"] != "NOT" and AUTO_CONFIG["exceldir"] != "NOT"
-)
 
+def init_automation():
+    # config check
+    if AUTO_CONFIG["slected_date"] == "NOT":
+        show_slected_value(Label["app"], "날짜를 먼저 선택하세요")
+        return
 
-def init_automation(config):
-    # window date
-    date = config["slected_date"].split("-")
-    date_tuple = (date[0], date[1], date[2], 0, 0, 0)
-    _win_set_time(date_tuple)
+    if AUTO_CONFIG["exceldir"] == "NOT":
+        show_slected_value(Label["app"], "파일을 먼저 선택하세요")
+
+        return
+
+    # set window date
+    date = list(AUTO_CONFIG["slected_date"].replace(
+        " ", "").strip().split("."))
+    date_tuple = (int(f'20{date[0]}'), int(date[1]), int(date[2]), 0, 0, 0, 0)
+    win_set_time(date_tuple)
 
     sleep(2.5)
+
     # automation process
-    id_list = get_column(config["exceldir"], "E")
-    automation(id_list, 1.75)
+    automation(exceldir=AUTO_CONFIG["exceldir"],
+               id_location="E", iter_time=1.75)
 
     return
 
 
 def set_automation_config(config, slected_date="NOT", exceldir="NOT"):
-    config["slected_date"] = slected_date
-    config["exceldir"] = exceldir
-    return
+    if slected_date == "NOT":
+        config["exceldir"] = exceldir
+        return
+    if exceldir == "NOT":
+        config["slected_date"] = slected_date
+        return
 
 
 def show_slected_value(label, value):
@@ -45,6 +55,7 @@ def set_slected_date():
     show_slected_value(Label["calendar"], f"window 날짜: {slected_date} 설정")
 
     set_automation_config(AUTO_CONFIG, slected_date=slected_date)
+
     return
 
 
@@ -53,6 +64,7 @@ def set_excel_dir():
     show_slected_value(Label["filedir"], f"파일 위치: {filedir}")
 
     set_automation_config(AUTO_CONFIG, exceldir=filedir)
+
     return
 
 
@@ -60,34 +72,39 @@ def set_excel_dir():
 
 # App
 App = Tk()
-app_config = {"title": "식권 입력", "size": "640x400"}
+app_config = {"title": "식권 입력", "size": "500x500"}
 App.title(app_config["title"])
 App.geometry(app_config["size"])
-App.resizable(False, False)
+App.resizable(True, True)
 
 # Label
 Label = {
-    "app": Label(App, text="식권 입력", padx=5, pady=5),
-    "calendar": Label(App, text="날짜를 선택하세요", padx=5, pady=5),
-    "filedir": Label(App, text="파일을 선택하세요", padx=5, pady=5),
+    "app": Label(App, text="식권 입력", padx=12, pady=10, relief="solid", borderwidth=2),
+    "filedir": Label(App, text="A. 파일을 선택하세요", padx=5, pady=5),
+    "calendar": Label(App, text="B. 날짜를 선택하세요", padx=5, pady=5),
 }
-Label["app"].pack()
-Label["calendar"].pack()
-Label["filedir"].pack()
 
 # Button
 Button = {
     "date": Button(App, command=set_slected_date, text="날짜 선택", padx=5, pady=5),
-    "file": Button(App, command=load_dir, text="액셀 파일 선택", padx=5, pady=5),
+    "file": Button(App, command=set_excel_dir, text="액셀 파일 선택", padx=5, pady=5),
     "automation": Button(App, command=init_automation, text="자동화 시작", padx=5, pady=5),
 }
-Button["date"].pack()
-Button["file"].pack()
-if is_config_setup_finishied == True:
-    Button["automation"].pack()
 
 # Calendar
-calendar = Calendar(App)
+calendar = CalenderUI(App)
+
+# Add Component
+Label["app"].pack()
+
+Label["filedir"].pack()
+Button["file"].pack()
+
+Label["calendar"].pack()
+Button["date"].pack()
+
+Button["automation"].pack()
+calendar.pack(pady=10)
 
 # App initialize
 App.mainloop()
--- src/automation.py
+++ src/automation.py
@@ -1,26 +1,36 @@
 import pyautogui as pg
-import time
-
+from tkinter import messagebox
+from time import sleep
+from excel import get_column
 
 BUTTON_POS = {"x": 174, "y": 204}
 
 WINDOW_TITLE = "노인복지종합시스템PLUS"
 
 
-def automation(auto_list, iter_time):
-    focus_window = pg.getWindowsWithTitle(WINDOW_TITLE)[0]
-    focus_window.maximize()
+def automation(exceldir, id_location, iter_time):
+    try:
+        focus_window = pg.getWindowsWithTitle(WINDOW_TITLE)[0]
+        focus_window.maximize()
 
-    if focus_window.isActive == False:
-        focus_window.activate()
+        if focus_window.isActive == False:
+            focus_window.activate()
 
-    time.sleep(1)
+        # extract id list
+        id_list = get_column(exceldir, id_location)
 
-    for list in auto_list:
-        pg.moveTo(
-            focus_window.left + BUTTON_POS["x"], focus_window.top + BUTTON_POS["y"]
-        )
-        pg.click(focus_window.top + BUTTON_POS["x"], focus_window.top + BUTTON_POS["y"])
-        pg.write(list)
-        pg.press("enter")
-        time.sleep(iter_time)
+        for id in id_list:
+            pg.moveTo(
+                focus_window.left +
+                BUTTON_POS["x"], focus_window.top + BUTTON_POS["y"]
+            )
+            pg.click(focus_window.top +
+                     BUTTON_POS["x"], focus_window.top + BUTTON_POS["y"])
+            pg.write(id)
+            pg.press("enter")
+
+            sleep(iter_time)
+
+    except:
+        messagebox.showerror("error", "회원 회비입금관리가 최대로 열려 있는지 확인하세요")
+        return
