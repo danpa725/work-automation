1de128643d604a73fcf134675966ff6c377e3ea7
mapo june <danpa725@cau.ac.kr>
2022-06-09 15:24:26 +0900

    Add: main automation feature .

---
+++ src/excel.py
@@ -0,0 +1,7 @@
+from openpyxl import load_workbook
+
+
+def get_column(dir, row_location):
+    excel_data = load_workbook(dir, data_only=True)["Sheet1"]
+
+    return list(filter(lambda x: x != None and "-" in x, map(lambda x: x.value, excel_data[row_location])))
