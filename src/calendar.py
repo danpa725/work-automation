57198e3bcaae94c1b908d14336728adaf7f7ce87
June@MacBook <danpa725@cau.ac.kr>
2022-06-10 13:40:42 +0900

    Add: calendar ui util .

---
+++ src/calendar.py
@@ -0,0 +1,21 @@
+from tkcalendar import Calendar
+from datetime import date
+
+
+def get_current_date():
+    curr = date.today()
+    return {"year": curr.year, "month": curr.month, "day": curr.day}
+
+
+current_date = get_current_date()
+
+
+def Calender(app):
+    return Calendar(
+        app,
+        selectmode="day",
+        year=current_date["year"],
+        month=current_date["month"],
+        day=current_date["day"],
+        locale="ko_KR",
+    )
