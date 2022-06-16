cea6235a714898bed7c262d0c32bbf61f7c38c4e
mapo june <danpa725@cau.ac.kr>
2022-06-16 10:31:32 +0900

    Update: excel data extraction error handler .

--- src/excel.py
+++ src/excel.py
@@ -1,13 +1,19 @@
+from tkinter import messagebox
 from openpyxl import load_workbook
 
 
 def get_column(directory: str, row_location: str):
-    excel_data = load_workbook(directory, data_only=True)["Sheet1"]
-    column_data = list(
-        filter(
-            lambda x: x != None and "-" in x,
-            map(lambda x: x.value, excel_data[row_location]),
+    try:
+        excel_data = load_workbook(directory, data_only=True)["Sheet1"]
+        column_data = list(
+            filter(
+                lambda x: x != None and "-" in x,
+                map(lambda x: x.value, excel_data[row_location]),
+            )
         )
-    )
 
-    return column_data
+        return column_data
+    except:
+        messagebox.showerror(
+            "error", "액셀 파일 데이터가 유효한지 확인하세요, 아이디는 XXXX-XXXXX 형식의 순수 문자열이어야 합니다.")
+        return
