340e89ca63457406e0ce6f3485430df30418a04f
mapo june <danpa725@cau.ac.kr>
2022-06-16 10:31:53 +0900

    Update: automation logic .

--- src/automation.py
+++ src/automation.py
@@ -1,14 +1,29 @@
+from typing import Dict
 import pyautogui as pg
 from tkinter import messagebox
 from time import sleep
 from excel import get_column
 
-BUTTON_POS = {"x": 174, "y": 204}
+START_BUTTON_POS = {"x": 79, "y": 156}
+MAX_WINDOW_SIZE_POS = {
+    "x": 820, "y": 65
+}
+INPUT_BUTTON_POS = {"x": 160, "y": 200}
 
 WINDOW_TITLE = "노인복지종합시스템PLUS"
 
 
-def automation(exceldir, id_location, iter_time):
+def move_click(window_object: any, pos: Dict[str, str]):
+    pg.moveTo(window_object.left + pos["x"], window_object.top + pos["y"])
+    pg.click(window_object.top + pos["x"], window_object.top + pos["y"])
+
+
+def move_dbclick(window_object: any, pos: Dict[str, str]):
+    pg.moveTo(window_object.left + pos["x"], window_object.top + pos["y"])
+    pg.doubleClick(window_object.top + pos["x"], window_object.top + pos["y"])
+
+
+def automation(exceldir: str, id_location: str, iter_time: int):
     try:
         focus_window = pg.getWindowsWithTitle(WINDOW_TITLE)[0]
         focus_window.maximize()
@@ -18,16 +33,29 @@ def automation(exceldir, id_location, iter_time):
 
         # extract id list
         id_list = get_column(exceldir, id_location)
+        if(len(id_list) == 0):
+            messagebox.showerror("error", "해당 열에 데이터가 없습니다.")
+            return
+
+        move_click(focus_window, START_BUTTON_POS)
+        sleep(3.5)
+
+        move_dbclick(focus_window, MAX_WINDOW_SIZE_POS)
+        sleep(2.5)
 
         for id in id_list:
-            pg.moveTo(
-                focus_window.left +
-                BUTTON_POS["x"], focus_window.top + BUTTON_POS["y"]
-            )
-            pg.click(focus_window.top +
-                     BUTTON_POS["x"], focus_window.top + BUTTON_POS["y"])
+            move_click(focus_window, INPUT_BUTTON_POS)
+
+            pg.press("right", presses=10)
+            pg.press("backspace", presses=11)
+
             pg.write(id)
+
             pg.press("enter")
+            
+            sleep(0.5)
+            
+            pg.press("esc")
 
             sleep(iter_time)
 
